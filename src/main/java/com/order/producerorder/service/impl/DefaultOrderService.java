e48a7bed16230c3c83b103dbc6688894274c7f47
jaymish <jaymishptl@gmail.com>
2022-03-02 21:41:02 -0800

    docker and Kafka working perfectly

--- src/main/java/com/order/producerorder/service/impl/DefaultOrderService.java
+++ src/main/java/com/order/producerorder/service/impl/DefaultOrderService.java
@@ -7,6 +7,7 @@ import com.order.producerorder.model.Orders;
 import com.order.producerorder.repo.OrderRepository;
 import com.order.producerorder.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -15,6 +16,9 @@ import java.util.List;
 @Service
 public class DefaultOrderService implements OrderService {
     //List<Weather> list=new LinkedList<>();
+
+    @Value("${url.to.consumer}")
+    private String url;
 
     private OrderRepository orderRepository;
     private RestTemplate restTemplate;
@@ -32,28 +36,28 @@ public class DefaultOrderService implements OrderService {
     @Override
     public boolean createOrders(Orders orders) throws JsonProcessingException {
 
-        return restTemplate.postForObject("http://localhost:8081/createOrder", orders, boolean.class);
+        return restTemplate.postForObject(url+"/createOrder", orders, boolean.class);
 
     }
 
     @Override
     public List<Orders> getAllOrders() {
         //List<Orders> ordersList= (List<Orders>) orderRepository.findAll();
-        return restTemplate.getForObject("http://localhost:8081/getOrder", ordersList.getClass());
+        return restTemplate.getForObject(url+"/getOrder", ordersList.getClass());
     }
 
     @Override
     public Orders getOrdersById(String id) {
-        return restTemplate.postForObject("http://localhost:8081/getById", id,Orders.class);
+        return restTemplate.postForObject(url+"/getById", id,Orders.class);
     }
 
     @Override
     public List<Orders> getOrdersByZip(int zip) {
-        return restTemplate.postForObject("http://localhost:8081/getByzip", zip, ordersList.getClass());
+        return restTemplate.postForObject(url+"/getByzip", zip, ordersList.getClass());
     }
 
     @Override
     public Orders cancelOrder(String id) {
-        return restTemplate.postForObject("http://localhost:8081/cancel", id,Orders.class);
+        return restTemplate.postForObject(url+"/cancel", id,Orders.class);
     }
 }
